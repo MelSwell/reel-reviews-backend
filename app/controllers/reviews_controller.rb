7cf0b6ec5ea75f72eefe2a75f720f95337523996
Seth M Cohen <smcohen82@gmail.com>
2021-05-21 19:55:16 -0400

    Adds update action to reviews controller, adds params to snake case to application controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :find_review, only: [:show]
+  before_action :find_review, only: [:show, :update]
 
   def index
     @reviews = Review.where(user_id: params[:user_id])
@@ -18,10 +18,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: @review.errors.full_messages
+    end
+  end
+
   private
 
   def find_review
     @review = Review.find_by(id: params[:id], user_id: params[:user_id])
+  end
+
+  def review_params
+    params.permit(:user_id, :id, :written_review, :rating, :movie_id)
   end
 
 end
