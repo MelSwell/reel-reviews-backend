476616d3bf496a952d96060ab3ee35eae2aa4ef1
Seth M Cohen <smcohen82@gmail.com>
2021-05-30 13:59:32 -0400

    switching branches

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/search', to: 'tmdb_api#search'
   get '/recommendations', to: 'tmdb_api#recommendations'
+  post '/login', to: 'sessions#login'
   
   resources :movies
   resources :users do 
