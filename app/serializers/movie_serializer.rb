2dba04b55653dccb9597f731f0befdbd738bf281
Seth M Cohen <smcohen82@gmail.com>
2021-06-01 19:48:12 -0400

    adds reviews to movie serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,3 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :average_tmdb_rating, :poster_img, :director, :tmdb_id, :overview, :backdrop_img, :trailer, :cast, :genres, :keywords, :release_date
+  attributes :id, :title, :average_tmdb_rating, :poster_img, :director, :tmdb_id, :overview, :backdrop_img, :trailer, :cast, :genres, :keywords, :release_date, :reviews
 end
